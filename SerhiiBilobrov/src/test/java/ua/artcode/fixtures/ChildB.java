c0ec0a6dd73d1dc7c76c5b6d7a53f6ff60c138c5
presly808 <presly808@gmail.com>
2016-11-26 16:07:20 +0200

    fixtures examples

---
+++ SerhiiBilobrov/src/test/java/ua/artcode/fixtures/ChildB.java
@@ -0,0 +1,22 @@
+package ua.artcode.fixtures;
+
+import org.junit.Test;
+
+/**
+ * Created by serhii on 11/26/16.
+ */
+public class ChildB extends Parent {
+
+
+
+    @Test
+    public void test1(){
+        System.out.println("Test1B");
+    }
+
+    @Test
+    public void test2(){
+        System.out.println("Test2B");
+    }
+
+}
